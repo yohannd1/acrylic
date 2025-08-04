f150dd3dc96db1222ab5b4a76cdc89ffc77f626b
yohannd1 <mitonanan12@gmail.com>
2025-08-03 21:50:46 -0300

    an attempt at improving the CLI parsing, which might have not been all that successful

--- src/cli.rs
+++ src/cli.rs
@@ -1,117 +1,178 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
-#[derive(Debug, Clone)]
-pub struct Options {
-    pub katex_path: String,
-    pub backend: Backend,
-    pub input_path: PathBuf,
-    pub output_path: PathBuf,
+#[derive(Debug)]
+pub struct CliOption {
+    pub name: String,
+    pub short: String,
+    pub help: String,
+    pub has_arg: bool,
+    pub value: Option<String>,
 }
 
-#[derive(Debug, Clone, Copy)]
-pub enum Backend {
-    Html,
+impl CliOption {
+    pub fn is_specified(&self) -> bool {
+        self.value.is_some()
+    }
 }
 
-pub fn show_help(progname: &str) {
-    eprintln!("{progname}: parse and render acrylic files (.acr)");
-    eprintln!();
-    eprintln!("Usage: {progname} [OPTIONS] <FILE>");
-    eprintln!();
-    eprintln!("Options:");
-    eprintln!("  --help/-h: show this help message");
-    eprintln!("  --output/-o: the output file (defaults to stdout)");
-    eprintln!("  --backend/-b: the output backend (defaults to HTML)");
-    eprintln!("  --katex-path/-k: the KaTeX path (for the HTML backend)");
+#[derive(Debug)]
+pub struct CliArg {
+    pub name: String,
+    pub value: Option<String>,
 }
 
-// TODO: refactor this into a state machine. probably would be muuuch better.
-pub fn parse_options(args: &[String]) -> Result<Options, String> {
-    let progname = get_progname(&args).expect("program name not available");
+#[derive(Debug)]
+pub struct CliParser<'a> {
+    pub options: Vec<CliOption>,
+    pub args: Vec<CliArg>,
+    pub progname: &'a str,
+}
 
-    let mut katex_path = None;
-    let mut backend = Backend::Html;
-    let mut input_path = None;
-    let mut output_path = None;
-
-    fn is_option(arg: &str) -> bool {
-        arg.starts_with("-") && arg.len() > 1
+impl<'a> CliParser<'a> {
+    pub fn new(arg0: &'a str) -> Self {
+        Self {
+            options: Vec::new(),
+            args: Vec::new(),
+            progname: get_progname(arg0).expect("failed to get program name"),
+        }
     }
 
-    let get_arg_value = |i: &mut usize, arg: &str| -> Result<&str, String> {
-        match args.get(*i + 1).filter(|x| !is_option(x)) {
-            Some(a) => {
-                *i += 2;
-                Ok(a)
-            }
-            None => {
-                show_help(progname);
-                Err(format!("missing value for argument {arg}"))
-            }
-        }
-    };
+    pub fn add_option(&mut self, option: CliOption) {
+        let CliOption { name, short, .. } = &option;
+        assert!(name.starts_with("--") && name.len() >= 3);
+        assert!(short != "--" && short.starts_with("-") && short.len() == 2);
+        self.options.push(option);
+    }
 
-    let mut i = 1;
-    loop {
-        match args.get(i).map(String::as_str) {
-            Some("-h" | "--help") => {
-                show_help(progname);
-                return Err("".into());
+    pub fn add_arg(&mut self, arg: CliArg) {
+        self.args.push(arg);
+    }
+
+    pub fn get_option(&self, query: &str) -> Option<&CliOption> {
+        self.options
+            .iter()
+            .find(|o| o.short == query || o.name == query)
+    }
+
+    pub fn get_option_mut(&mut self, query: &str) -> Option<&mut CliOption> {
+        self.options
+            .iter_mut()
+            .find(|o| o.short == query || o.name == query)
+    }
+
+    pub fn get_arg(&mut self, query: &str) -> Option<&CliArg> {
+        self.args.iter().find(|a| query == a.name)
+    }
+
+    pub fn get_arg_mut(&mut self, query: &str) -> Option<&mut CliArg> {
+        self.args.iter_mut().find(|a| query == a.name)
+    }
+
+    pub fn parse_args<I>(&mut self, args: I) -> Result<(), String>
+    where
+        I: IntoIterator,
+        <I as IntoIterator>::Item: AsRef<str>,
+    {
+        let mut cur_option: Option<String> = None;
+        let mut skip_opt = false;
+        let mut arg_i = 0;
+
+        fn is_option(arg: &str, skip_opt: bool) -> bool {
+            !skip_opt && arg.starts_with("-") && arg != "-" && arg != "--"
+        }
+
+        fn is_skip_opt(arg: &str, skip_opt: bool) -> bool {
+            !skip_opt && arg == "--"
+        }
+
+        let mut it = args.into_iter().peekable();
+        while let Some(arg) = it.peek() {
+            let arg: &str = arg.as_ref();
+
+            if is_skip_opt(arg, skip_opt) {
+                skip_opt = true;
+                continue;
             }
-            Some("-k" | "--katex-path") => {
-                let value = get_arg_value(&mut i, "--katex-path")?;
-                katex_path = Some(value.to_owned());
-            }
-            Some("-o" | "--output") => {
-                let value = get_arg_value(&mut i, "--output")?;
-                output_path = Some(value)
-                    .filter(|x| *x != "-")
-                    .map(Path::new)
-                    .map(Path::to_owned);
-            }
-            Some("-b" | "--backend") => {
-                let value = get_arg_value(&mut i, "--backend")?;
-                backend = match value {
-                    "html" => Backend::Html,
-                    _ => {
-                        return Err(format!("unknown backend {value:?}"));
-                    }
-                };
-            }
-            Some(x) if is_option(x) => {
-                show_help(progname);
-                return Err(format!("unknown option {x:?}"));
-            }
-            Some(x) => {
-                if input_path.is_some() {
-                    show_help(progname);
-                    return Err(format!("unknown argument {x:?}"));
+
+            if let Some(opt) = &cur_option {
+                if is_option(arg, skip_opt) {
+                    return Err(self.error_help(format!("not enough args for option {opt}")));
                 } else {
-                    if x == "-" {
-                        input_path = Some(PathBuf::from("/dev/stdin"));
-                    } else {
-                        input_path = Some(PathBuf::from(x));
+                    let opt_data = self.get_option_mut(opt).unwrap();
+                    assert!(opt_data.has_arg && opt_data.value.is_none());
+                    opt_data.value = Some(arg.to_owned());
+                    cur_option = None;
+                }
+            } else {
+                if is_option(arg, skip_opt) {
+                    if arg == "-h" || arg == "--help" {
+                        self.help();
+                        return Err("".into());
                     }
-                    i += 1;
+
+                    let opt_data = match self.get_option_mut(arg) {
+                        Some(x) => x,
+                        None => return Err(self.error_help(format!("unknown option: {arg}"))),
+                    };
+                    if opt_data.is_specified() {
+                        return Err(self.error_help(format!("option {arg} passed more than once")));
+                    }
+
+                    if opt_data.has_arg {
+                        cur_option = Some(arg.to_owned());
+                    } else {
+                        // empty string just to signal it has been provided
+                        opt_data.value = Some(String::new());
+                    }
+                } else {
+                    let cur_arg = match self.args.get_mut(arg_i) {
+                        Some(x) => x,
+                        None => return Err(self.error_help(format!("too many arguments"))),
+                    };
+                    cur_arg.value = Some(arg.to_owned());
+                    arg_i += 1;
                 }
             }
-            None => break,
+            _ = it.next();
         }
+
+        if let Some(opt) = cur_option {
+            return Err(self.error_help(format!("missing argument for {opt:?}")));
+        }
+
+        if arg_i != self.args.len() {
+            return Err(self.error_help(format!("too few arguments")));
+        }
+
+        Ok(())
     }
 
-    Ok(Options {
-        katex_path: katex_path.unwrap_or_else(String::new),
-        backend,
-        input_path: input_path.ok_or_else(|| {
-            show_help(progname);
-            format!("missing FILE argument")
-        })?,
-        output_path: output_path.unwrap_or_else(|| PathBuf::from("/dev/stdout")),
-    })
+    pub fn error_help(&self, error: String) -> String {
+        self.help();
+        return error;
+    }
+
+    pub fn help(&self) {
+        let Self { progname, .. } = self;
+        eprintln!("{progname}: parse and render acrylic files (.acr)");
+        eprintln!();
+        eprint!("Usage: {progname} [OPTIONS]");
+        for arg_def in &self.args {
+            eprint!(" <{}>", arg_def.name);
+        }
+        eprintln!("\nOptions:");
+        eprintln!("  --help/-h: show this help message");
+        for opt in &self.options {
+            let CliOption {
+                name, short, help, ..
+            } = opt;
+            eprintln!("  {name}/{short}: {help}");
+        }
+    }
 }
 
-fn get_progname(args: &[String]) -> Option<&str> {
-    args.get(0)
+fn get_progname(arg0: &str) -> Option<&str> {
+    Some(arg0)
         .map(Path::new)
         .and_then(Path::file_name)
         .and_then(|x| x.try_into().ok())
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ mod cli;
 mod html;
 mod parser;
 
-use crate::cli::Backend;
+use crate::cli::{CliArg, CliOption, CliParser};
 use crate::html::{write_html, HtmlOptions};
 use crate::parser::parse;
+use std::path::PathBuf;
 
 // TODO: make tests for stage1 - conditions where each type of term parses
 //
@@ -17,6 +18,19 @@ use crate::parser::parse;
 // becomes its own kind of line; task prefixes can affect the style of the entire line; bullet point
 // styles; and i might be able to more efficiently handle spacing at the end of fold blocks (they
 // shouldn't be hidden inside it) ...
+
+#[derive(Debug, Clone)]
+pub struct Options {
+    pub katex_path: String,
+    pub backend: Backend,
+    pub input_path: PathBuf,
+    pub output_path: PathBuf,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Backend {
+    Html,
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -33,7 +47,7 @@ fn main() {
 }
 
 fn app(args: &[String]) -> Result<(), String> {
-    let options = cli::parse_options(args)?;
+    let options = parse_options(args)?;
 
     let file_contents = std::fs::read_to_string(options.input_path)
         .map_err(|e| format!("failed to open input file: {:?}", e))?;
@@ -54,4 +68,72 @@ fn app(args: &[String]) -> Result<(), String> {
     }
 
     Ok(())
+}
+
+fn parse_options(args: &[String]) -> Result<Options, String> {
+    let mut p = CliParser::new(args[0].as_str());
+    p.add_arg(CliArg {
+        name: "FILE".into(),
+        value: None,
+    });
+    p.add_option(CliOption {
+        name: "--output".into(),
+        short: "-o".into(),
+        help: "the output file (defaults to stdout)".into(),
+        has_arg: true,
+        value: None,
+    });
+    p.add_option(CliOption {
+        name: "--backend".into(),
+        short: "-b".into(),
+        help: "the output backend (defaults to HTML)".into(),
+        has_arg: true,
+        value: None,
+    });
+    p.add_option(CliOption {
+        name: "--katex-path".into(),
+        short: "-k".into(),
+        help: "the KaTeX path (for the HTML backend)".into(),
+        has_arg: true,
+        value: None,
+    });
+
+    p.parse_args(&args[1..])?;
+
+    let backend = match p
+        .get_option("--backend")
+        .and_then(|x| x.value.as_deref())
+    {
+        Some("html") | None => Backend::Html,
+        Some(x) => return Err(p.error_help(format!("Unknown backend {x:?}"))),
+    };
+
+    let katex_path = p
+        .get_option("--katex-path")
+        .and_then(|x| x.value.clone())
+        .unwrap_or_else(String::new);
+
+    let input_path = match p
+        .get_arg("FILE")
+        .and_then(|x| x.value.as_deref())
+        .unwrap()
+    {
+        "-" => PathBuf::from("/dev/stdin".to_string()),
+        other => PathBuf::from(other),
+    };
+
+    let output_path = match p
+        .get_option("--output")
+        .and_then(|x| x.value.as_deref())
+    {
+        Some("-") | None => PathBuf::from("/dev/stdout"),
+        Some(other) => PathBuf::from(other),
+    };
+
+    Ok(Options {
+        katex_path,
+        backend,
+        input_path,
+        output_path,
+    })
 }
